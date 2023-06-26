8f30873ceb0b3b98d655e5b2793e29602a3bf7cf
nocteln <eliottmieze@gmail.Com>
2023-06-26 15:07:53 +0200

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       host: "87.106.234.10",
       ref: "origin/main",
       repo: "https://github.com/Nocteln/site-perso",
-      path: "/root/site-perso",
+      path: "/root/site-perso/src",
       "pre-deploy-local": "",
       "post-deploy":
         "npm install && pm2 reload ecosystem.config.js --env production",
